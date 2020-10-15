cd2aa837ff0a9d64698b462f4599f39e9893dbd6
ChihayaMorita <chihayamorita1027@gmail.com>
2020-10-15 16:54:49 +0900

    modify
     # what
    sp画面のcss作成

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -4,7 +4,7 @@ $(function(){
       var rightVal = 0;
       if($(this).hasClass("open")) {
           // 位置を移動させメニューを開いた状態にする
-          rightVal = -300;
+          rightVal = -500;
           // メニューを開いたら次回クリック時は閉じた状態になるよう設定
           $(this).removeClass("open");
       } else {
