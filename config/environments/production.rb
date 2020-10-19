1c8bcf5b21a0d78d7d4de1b9ace733d70e8da6ad
ChihayaMorita <chihayamorita1027@gmail.com>
2020-10-19 20:16:50 +0900

    modify
    cssの読み込み修正
    jsの読み込み修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,6 @@ Rails.application.configure do
   config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
-  config.assets.precompile += %w(*.js *.less *.css)
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
